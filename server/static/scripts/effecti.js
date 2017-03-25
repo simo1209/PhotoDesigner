397ceedb71dcf73a2a673f5e7f3bcd4e98b0cbc0
Александър Стоичков <sashostoichkov@gmail.com>
2017-03-25 15:43:37 +0200

    99% success

--- server/static/scripts/effecti.js
+++ server/static/scripts/effecti.js
@@ -1,10 +1,16 @@
 $(function() {
   $(".photo").fadeOut(0);
-  $(".photo").fadeIn(1000);
+  $(".photo").fadeIn(2000);
 });
 
 $(function() {
   $(".text").fadeOut(0);
   $(".text").slideUp(1000);
   $(".text").slideDown(1000);
+});
+
+$(function() {
+  $("#opisanie").fadeOut(0);
+  $("#opisanie").slideUp(1000);
+  $("#opisanie").slideDown(1000);
 });
