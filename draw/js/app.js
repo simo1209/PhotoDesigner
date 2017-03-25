3639f37845c1134d0740a2f362ab7bf03f409419
simo1209 <simeongeorgiev09@gmail.com>
2017-03-25 11:28:08 +0200

    Resizing canvas

--- draw/js/app.js
+++ draw/js/app.js
@@ -1,21 +1,34 @@
 let currentEl;
-let layers=new Array();
-let uiObjects={};
-let currentBG="#000000";
+let layers = new Array();
+let uiObjects = {};
+let currentBG = "#000000";
 
-function containCoords(corners,coords){
-    return corners[0].x <= coords.x && corners[0].y <= coords.y &&  corners[1].x>=coords.x && corners[1].y>=coords.y;
+function containCoords(corners, coords) {
+    return corners[0].x <= coords.x && corners[0].y <= coords.y && corners[1].x >= coords.x && corners[1].y >= coords.y;
 }
 
-function setup(){
+function canvasResize() {
+    //resizeCanvas(500,200);
+    resizeCanvas(uiObjects.canvasWidth.value(),uiObjects.canvasHeight.value());
+    background(currentBG);
+    for (let i = 0; i < layers.length; i++) {
+        layers[i].draw();
+    }
+    //resizeCanvas();
+}
 
-    
-    uiObjects.canvas=createCanvas(600, 400);
+function setup() {
+
+
+    uiObjects.canvas = createCanvas(600, 400);
+    uiObjects.canvasDiv = createDiv("");
+    uiObjects.canvasWidth = createInput(width).input(canvasResize).parent(uiObjects.canvasDiv);
+    uiObjects.canvasHeight = createInput(height).input(canvasResize).parent(uiObjects.canvasDiv);
     background(currentBG);
     textSize(48);
     textAlign(CENTER, CENTER);
 
-    let inpTxt="Hello World";
+    /*let inpTxt="Hello World";
     currentEl = new Layer(
         [
             createVector(width/2-textWidth(inpTxt)/2,height/2-textSize()/2),
@@ -25,36 +38,40 @@ function setup(){
         "text"
     );
     
-    currentEl.draw();
-    layers.push(currentEl);
+    currentEl.draw();*/
+    //layers.push(currentEl);
 }
 
-function mousePressed(){
+function draw() {
+
+}
+
+function mousePressed() {
     background(currentBG);
-    for(let i=0;i<layers.length;i++){
+    for (let i = 0; i < layers.length; i++) {
         layers[i].draw();
-        if (            
+        if (
             layers[i].getUpperCorner().x <= mouseX &&
             layers[i].getDownCorner().x >= mouseX &&
             layers[i].getUpperCorner().y <= mouseY &&
             layers[i].getDownCorner().y >= mouseY
-        ){
-            currentEl=layers[i];
+        ) {
+            currentEl = layers[i];
             currentEl.drawBorder();
         }
     }
 }
 
-function mouseDragged(){
+function mouseDragged() {
     background(currentBG);
-    for(let i=0;i<layers.length;i++){
+    for (let i = 0; i < layers.length; i++) {
         layers[i].draw();
-        if (            
-            containCoords([layers[i].getUpperCorner(),layers[i].getDownCorner()],createVector(mouseX,mouseY))
-        ){
-            currentEl=layers[i];
+        if (
+            containCoords([layers[i].getUpperCorner(), layers[i].getDownCorner()], createVector(mouseX, mouseY))
+        ) {
+            currentEl = layers[i];
             currentEl.setCenter(
-                createVector(mouseX,mouseY)
+                createVector(mouseX, mouseY)
             );
             currentEl.drawBorder();
         }
